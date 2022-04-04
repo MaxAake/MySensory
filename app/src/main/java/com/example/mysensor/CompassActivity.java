4562691185c96b5dc90cb59f8ec05e82d5350da8
MaxAake <maxgustafsson.ugglum@gmail.com>
2022-04-04 13:29:41 +0200

    swapped from TYPE_ORIENTATION to getOrientation

--- app/src/main/java/com/example/mysensor/CompassActivity.java
+++ app/src/main/java/com/example/mysensor/CompassActivity.java
@@ -1,5 +1,6 @@
 package com.example.mysensor;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -7,19 +8,30 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import java.util.Random;
+
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
 
     // define the display assembly compass picture
+    private int counter = 0;
+    private final int averageCount = 7;
+    private double[] angles = new double[averageCount];
+    private double angle;
+    Random rand = new Random();
     private ImageView image;
-    float[] acc;
-    float[] mag;
+    float[] rot = new float[9];
+    float[] mag = new float[3];
+    float[] grav = new float[3];
+    float[] orient = new float[3];
     // record the compass picture angle turned
     private float currentDegree = 0f;
 
@@ -48,9 +60,7 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         super.onResume();
 
         // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
-                SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
                 SensorManager.SENSOR_DELAY_GAME);
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
                 SensorManager.SENSOR_DELAY_GAME);
@@ -68,7 +78,7 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     public void onSensorChanged(SensorEvent event) {
 
         // get the angle around the z-axis rotated
-        if(event.sensor.getStringType().equals(Sensor.STRING_TYPE_ORIENTATION)) {
+        /*if(false ){ //event.sensor.getStringType().equals(Sensor.STRING_TYPE_ORIENTATION)) {
             float division = (float) 10.0;
             float degree = Math.round(event.values[0] * 10.0);
             degree = degree / division;
@@ -94,13 +104,22 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
             image.startAnimation(ra);
             currentDegree = -degree;
         }
-        else if ((event.sensor.getStringType().equals(Sensor.STRING_TYPE_ACCELEROMETER))){
-            acc = event.values;
+
+         */
+        if ((event.sensor.getStringType().equals(Sensor.STRING_TYPE_GRAVITY))){
+            grav = event.values;
         }
 
         else if ((event.sensor.getStringType().equals(Sensor.STRING_TYPE_MAGNETIC_FIELD))){
-            acc = event.values;
+            mag = event.values;
         }
+
+        SensorManager.getRotationMatrix(rot, null, grav, mag);
+        SensorManager.getOrientation(rot, orient);
+        angle = orient[0];
+        angle = (Math.toDegrees(angle) + 360) % 360.0;
+        image.setRotation((float) -angle);
+        tvHeading.setText("Heading: " + Double.toString(angle) + " degrees");
 
     }
 
